e0ce267323d7b1e583b54a80bf6aa7f86f688b7c
Панов Антон <hightemp.unknown@gmail.com>
2023-01-16 22:59:38 +0300

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 docker build -t ru_paraphraser .
+docker run ru_paraphraser "Наше предприятие вышло на новый уровень."
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-input="/home/requerements.txt"
-while IFS= read -r line
-do
-    pip install $line
-done < "$input"
